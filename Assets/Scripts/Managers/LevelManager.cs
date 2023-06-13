8be3d7a0287630443f62a0c7ee166464a9265d6d
Kyle Dixon <90637263+KyleDixonVO@users.noreply.github.com>
2023-06-12 21:44:37 -0300

    Created methods to swap the active canvas and scene

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
 
 public class LevelManager : MonoBehaviour
 {
+
+    public enum Scenes 
+    { 
+        MainMenu,
+        LevelOne,
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +23,14 @@ public class LevelManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SwapActiveScene(Scenes activeScene, bool unloadOtherScenes)
+    {
+        for (int i = 0; i < Enum.GetNames(typeof(Scenes)).Length; i++)
+        {
+            if ((int)activeScene != i && unloadOtherScenes) SceneManager.UnloadSceneAsync(i);
+            else if ((int)activeScene == i) SceneManager.LoadScene((int)activeScene);
+        }
     }
 }
--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+
+    public enum Canvases 
+    { 
+        MainMenu,
+        Gameplay,
+        Pause,
+        Settings,
+        NextLevel,
+        GameOver
+    }
+
+    public Canvas[] canvasComponents;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +27,14 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SwapActiveCanvas(Canvases canvas, bool disableOtherCanvases)
+    {
+        for (int i = 0; i < canvasComponents.Length; i++)
+        {
+            if ((int)canvas != i && disableOtherCanvases) canvasComponents[i].gameObject.SetActive(false);
+            else if ((int)canvas == i) canvasComponents[i].gameObject.SetActive(true);
+        }
     }
 }
